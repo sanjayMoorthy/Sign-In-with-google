9f5f2cdd236abf7dcd6ecdc3c32aeecd7dbd2c4e
sanjayMoorthy <sanjaysdckap@gmail.com>
2023-08-14 14:35:18 +0530

    ok

--- authentication.js
+++ authentication.js
@@ -54,8 +54,6 @@ signupButton.addEventListener('click', () => {
 
 
 
-
-
     // signInWithPopup(auth, provider)
     //     .then((result) => {
     //         // This gives you a Google Access Token. You can use it to access the Google API.
